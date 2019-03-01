9745d6aef8697293519aa4636e03300e3458f088
Loumstar <louis.manestar@gmail.com>
2019-03-01 18:24:15 +0000

    Completed exercise 7

---
+++ python_exercises7.py
@@ -0,0 +1,83 @@
+import re
+
+print("--- Task A")
+
+def fib(a, b): 
+    return a + b
+
+def nth_fibonacci(n):
+    a = b = 1
+    for _ in range(n - 1):
+        a, b = b, fib(a, b)
+    return a
+
+print(nth_fibonacci(22))
+
+print("--- Task B")
+
+m = [
+    [24, 67, 81],
+    [10, 3, 28],
+    [63, 55, 17]
+]
+
+def swap_elements(a, b, m):
+    #where a and b are tuples of ij index notation
+    m[a[0]][a[1]], m[b[0]][b[1]] = m[b[0]][b[1]], m[a[0]][a[1]]
+    return m
+
+val67 = (0, 1)
+val17 = (2, 2)
+val81 = (0, 2)
+val63 = (2, 0)
+
+swap_elements(val67, val17, m)
+swap_elements(val81, val63, m)
+
+m[1][1] = m[0][0] + m[1][2]
+
+print(m[2][2])
+
+print("--- Task C")
+
+def create_flag(n):
+    m = []
+    for j in range(n):
+        sub_m = [0] * n
+        sub_m[j] = 1
+        m.append(sub_m)
+    return m
+
+print("--- Task D")
+
+with open("./exercise7/MatA.txt", 'r') as f:
+    mat_a_lst = [int(x) for x in re.findall('\d+', f.read())]
+
+with open("./exercise7/MatB.txt", 'r') as f:
+    mat_b_lst = [int(x) for x in re.findall('\d+', f.read())]
+
+def read_matrices(lst, n, m):
+    matrices = []
+    matrix = []
+    matrix_index = 0
+    list_length = len(lst)
+    while (matrix_index * m * n) < list_length:
+        for j in range(m):
+            i = (matrix_index * m * n) + (j * m)
+            matrix.append(lst[i:i+n])
+        matrices.append(matrix)
+        matrix_index += 1
+    return matrices
+
+mat_a = read_matrices(mat_a_lst, 60, 60)[0]
+mat_b = read_matrices(mat_b_lst, 60, 60)[0]
+
+print(mat_b[25][36])
+
+def sum_matrix(a, b):
+    m = []
+    for j in range(len(a)):
+        m.append(list(map(lambda x, y: x + y, a[j], b[j])))
+    return m
+
+print(sum_matrix(mat_a, mat_b)[24][19])
